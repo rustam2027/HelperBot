aad20fe21a984ac42f3742effea9c0fe6c32e529
Taisia Kashintseva <the_smartest_peach@mail.ru>
2024-02-17 16:07:01 +0700

    Half made students list

--- bot-main/Group.py
+++ bot-main/Group.py
@@ -1,26 +1,22 @@
 import Student
 from typing import List
+from typing import Tuple
 from dataclasses import dataclass
+import datetime
 
 
 @dataclass
 class Group:
     number: str
     students: List[Student]
+    courses: List[Tuple[str, str]]  # name + table_id
 
     def get_semester(self):
-        admission_year = self.number[0:1]
+        admission_year = self.number[0:2]
+
         # TODO: get correct semester
         return 2
 
     def get_course_list(self) -> list[str]:
         semester = self.get_semester()
-        match semester:
-            case 1:
-                return ['Python', 'C', 'ИСП']
-            case 2:
-                return ['Алгоритмы и структуры данных', 'АрхЭВМиНуП']
-            case 3:
-                pass
-            case 4:
-                pass
+
