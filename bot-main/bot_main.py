ffa78690676746b2b23b5b153acf55cc74d089a9
Taisia Kashintseva <the_smartest_peach@mail.ru>
2024-02-17 11:43:30 +0700

    Bot answers + KeyBoardButtons

--- bot-main/bot_main.py
+++ bot-main/bot_main.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import Message
+from telebot import types
 
 TOKEN: str = '6924911833:AAEkGGKgCG-F91EWpJqXOnB7XqYJvhQ0wlA'
 
@@ -7,14 +7,22 @@ bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['send pic'])
-def send_pic(message: Message):
+def send_pic(message: types.Message):
     bot.reply_to(message, "done. nice. hello")
 
 
 @bot.message_handler(commands=['start'])
-def start(message: Message):
+def start(message: types.Message):
     bot.send_message(message.from_user.id, "hello")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("🇷🇺 Русский")
+    btn2 = types.KeyboardButton('🇬🇧 English')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "🇷🇺 Выберите язык / 🇬🇧 Choose your language", reply_markup=markup)
 
 
 def __main__():
     bot.polling(none_stop=True, interval=0)
+
+
+bot.polling(none_stop=True, interval=0)
