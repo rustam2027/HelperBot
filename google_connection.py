a945a87099268f3cad6e0e053817e68fefbb5749
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2024-02-17 12:37:40 +0700

    Add connection and test reading

--- google_connection.py
+++ google_connection.py
@@ -1,18 +1,3 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START sheets_quickstart]
 import os.path
 
 from google.auth.transport.requests import Request
@@ -22,11 +7,11 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-SAMPLE_RANGE_NAME = "Class Data!A2:E"
+SAMPLE_SPREADSHEET_ID = "1mVc9THvtGtvRmK1tIaXkzxk2Cgy82BqWMWcRlO_PA6k"
+SAMPLE_RANGE_NAME = "Class DataA2:E3"
 
 
 def main():
@@ -78,4 +63,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-# [END sheets_quickstart]
