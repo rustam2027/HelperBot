07f81429a241d43b860282d5bb4481ca011eaabd
Luba Kolbasova <luba1607@gmail.com>
2024-02-17 17:53:49 +0700

    write work

--- Manager.py
+++ Manager.py
@@ -1,4 +1,3 @@
-from typing import List, Dict
 from typing import List, Dict, Tuple
 from Connection import Connection
 from Course import Course
@@ -13,19 +12,19 @@ class Manager:
     def __init__(self):
         self.connection = Connection()
         self.connection.connect()
-        self.groups = dict()
+        self.groups = {"22126": Group("", [], {"A": Course("A", [], "1mVc9THvtGtvRmK1tIaXkzxk2Cgy82BqWMWcRlO_PA6k", "")})}
         self.names_range = (5, 27)
         # TODO: Read groups from file
     
     def read_names(self, group: Group) -> None:
         start, end = self.names_range
-        random_course_table = group.courses.items()[0].table_id_students
+        random_course_table = group.courses["A"].table_id_students
         table = self.connection.read(f"A{start}:B{end}", random_course_table)
         names = []
         for name in table:
             if name[0] == "Вольнослушатели:":
                 break
-            names.append(Student(None, name[0], group.number, None))
+            names.append(Student("", name[0], group.number, []))
         group.students = names
 
     def addGroup(self, number: str, courses):
@@ -68,5 +67,7 @@ class Manager:
 
 if __name__ == "__main__":
     manager = Manager()
-    manager.read_tasks(Group("", [], {"A": Course("A", [], "1mVc9THvtGtvRmK1tIaXkzxk2Cgy82BqWMWcRlO_PA6k", "")}), "A")
-       
+    manager.read_tasks(manager.groups["22126"], "A")
+    manager.read_names(manager.groups["22126"])
+    student = Student("", "Васько Мария Богдановна", "22126", [])
+    manager.write(student, "1", "A")
