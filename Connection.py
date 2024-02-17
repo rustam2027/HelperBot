0e7c21fdd5e11cdf818f0a87372288da7441d748
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2024-02-17 13:26:34 +0700

    Add log funtion

--- Connection.py
+++ Connection.py
@@ -1,12 +1,17 @@
 import os.path
+import datetime
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 AUTH_SCOPE = ["https://www.googleapis.com/auth/spreadsheets"]
+
+
+def log(string: str) -> None:
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+    print(f"[{time}]: {string}")
 
 
 class Connection:
@@ -16,19 +21,25 @@ class Connection:
         self.service = None
 
     def connect(self):
+        log("Connecting")
         if os.path.exists("token.json"):
+            log("File 'token.json' found!")
             self.creds = Credentials.from_authorized_user_file("token.json", AUTH_SCOPE)
         if not self.creds or not self.creds.valid:
+            log("File 'token.json' not found or not valid!")
             if self.creds and self.creds.expired and self.creds.refresh_token:
+                log("Creds not valid")
                 self.creds.refresh(Request())
             else:
+                log("Creating new creds")
                 flow = InstalledAppFlow.from_client_secrets_file(
                     "credentials.json", AUTH_SCOPE
                 )
                 self.creds = flow.run_local_server(port=0)
 
-        with open("token.json", "w") as token:
-            token.write(self.creds.to_json())
+            with open("token.json", "w") as token:
+                log("Creating 'token.json'")
+                token.write(self.creds.to_json())
 
         self.service = build("sheets", "v4", credentials=self.creds)
 
