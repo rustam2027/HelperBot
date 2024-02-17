44bca24c302d0425a4e7e35799f2846f6eb41239
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2024-02-17 16:00:50 +0700

    Create function addGroup

--- Manager.py
+++ Manager.py
@@ -1,5 +1,6 @@
 from Typing import List
 from Connection import Connection
+from Group import Group
 
 
 class Manager:
@@ -9,3 +10,9 @@ class Manager:
     def __init__(self):
         self.connection = Connection()
         self.connection.connect()
+        self.groups = []
+        # TODO: Read groups from file
+
+    def addGroup(self, number: str, courses):
+        self.groups.append(Group(number, [], courses))
+
