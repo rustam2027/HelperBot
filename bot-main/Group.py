46115a9602f682b366d9801a136fb87d59ec9435
Taisia Kashintseva <the_smartest_peach@mail.ru>
2024-02-17 17:04:43 +0700

    New dto classes

--- bot-main/Group.py
+++ bot-main/Group.py
@@ -1,15 +1,14 @@
 import Student
 from typing import List
-from typing import Tuple
 from dataclasses import dataclass
-import datetime
+import Course
 
 
 @dataclass
 class Group:
     number: str
     students: List[Student]
-    courses: List[Tuple[str, str]]  # name + table_id
+    courses: List[Course]  # name + table_id
 
     def get_semester(self):
         admission_year = self.number[0:2]
@@ -17,6 +16,6 @@ class Group:
         # TODO: get correct semester
         return 2
 
-    def get_course_list(self) -> list[str]:
+    def get_course_list(self):
         semester = self.get_semester()
 
