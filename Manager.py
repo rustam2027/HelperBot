1d4a75473cdd8ef4fe5c9bfb3ae9fd36879a4c81
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2024-02-17 15:41:51 +0700

    Add init in Manager

--- Manager.py
+++ Manager.py
@@ -1,2 +1,11 @@
+from Typing import List
+from Connection import Connection
+
+
 class Manager:
-    
+    groups: List[Group]
+    connection: Connection
+
+    def __init__(self):
+        self.connection = Connection()
+        self.connection.connect()
